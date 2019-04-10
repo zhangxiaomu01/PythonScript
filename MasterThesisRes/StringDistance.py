10fda31a44e2b7dca5821ba882a864b4b834ccc3
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-04-10 17:09:18 -0400

    StringDistance

--- MasterThesisRes/StringDistance.py
+++ MasterThesisRes/StringDistance.py
@@ -1,5 +1,22 @@
-Ans = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
+import csv
+Ans = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
 Seq = ['A', 'E', 'B', 'C', 'D', 'F', 'G', 'I', 'J', 'K', 'H', 'L', 'M', 'N']
+
+filePath = 'E:\\Other\\PythonScript\\MasterThesisRes\\ParticipantsData.csv'
+'''
+f = open(filePath,'r')
+for x in f:
+    print(x)
+'''
+with open(filePath) as csv_file:
+    List = []
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    for x in csv_reader:
+        for y in x[2]:
+            if(y >= 'A' and y <= 'Z'):
+                List.append(y)
+    print(List)
+    Seq = List    
 
 
 lenS = len(Ans)
